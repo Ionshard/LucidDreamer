fa2868821fdb58a5c90b9f5cbe52e8f0f488a03f
Corey Ling <kasuko@gmail.com>
2009-12-13 00:43:58 -0500

    Added empty button click handlers.

--- src/com/ionshard/android/luciddreamer/activities/Main.java
+++ src/com/ionshard/android/luciddreamer/activities/Main.java
@@ -1,9 +1,12 @@
 package com.ionshard.android.luciddreamer.activities;
 
-import com.ionshard.android.luciddreamer.R;
-
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+import com.ionshard.android.luciddreamer.R;
 
 public class Main extends Activity {
     /** Called when the activity is first created. */
@@ -11,5 +14,29 @@ public class Main extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        ((Button)findViewById(R.id.main_alarm_button)).setOnClickListener(
+        	new OnClickListener(){
+				public void onClick(View arg0) {
+					//TODO Add Alarm Activity Call
+				}
+        	}
+        );
+        
+        ((Button)findViewById(R.id.main_reminder_button)).setOnClickListener(
+            	new OnClickListener(){
+    				public void onClick(View arg0) {
+    					//TODO Add Reminder Activity Call
+    				}
+            	}
+            );
+        
+        ((Button)findViewById(R.id.main_journal_button)).setOnClickListener(
+            	new OnClickListener(){
+    				public void onClick(View arg0) {
+    					//TODO Add Journal Activity Call
+    				}
+            	}
+            );
     }
 }
