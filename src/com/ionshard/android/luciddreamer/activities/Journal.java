8685ae276059ccec17d99aa73238ef951438ec8a
Corey Ling <kasuko@gmail.com>
2009-12-13 01:16:25 -0500

    Added Journal activity.

---
+++ src/com/ionshard/android/luciddreamer/activities/Journal.java
@@ -0,0 +1,13 @@
+package com.ionshard.android.luciddreamer.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Journal extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+}
