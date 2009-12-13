515b233f08ce7c8c4d83bbac9fd21805b2441110
Corey Ling <kasuko@gmail.com>
2009-12-13 01:14:44 -0500

    Updated handlers.
    Still no content.

--- src/com/ionshard/android/luciddreamer/activities/Main.java
+++ src/com/ionshard/android/luciddreamer/activities/Main.java
@@ -9,7 +9,6 @@ import android.widget.Button;
 import com.ionshard.android.luciddreamer.R;
 
 public class Main extends Activity {
-    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,7 +17,7 @@ public class Main extends Activity {
         ((Button)findViewById(R.id.main_alarm_button)).setOnClickListener(
         	new OnClickListener(){
 				public void onClick(View arg0) {
-					//TODO Add Alarm Activity Call
+					launchAlarm();
 				}
         	}
         );
@@ -26,7 +25,7 @@ public class Main extends Activity {
         ((Button)findViewById(R.id.main_reminder_button)).setOnClickListener(
             	new OnClickListener(){
     				public void onClick(View arg0) {
-    					//TODO Add Reminder Activity Call
+    					launchReminder();
     				}
             	}
             );
@@ -34,9 +33,21 @@ public class Main extends Activity {
         ((Button)findViewById(R.id.main_journal_button)).setOnClickListener(
             	new OnClickListener(){
     				public void onClick(View arg0) {
-    					//TODO Add Journal Activity Call
+    					launchJournal();
     				}
             	}
             );
+    }
+    
+    private void launchAlarm(){
+    	//TODO Alarm Activity Launch  	
+    }
+    
+    private void launchReminder(){
+    	//TODO Reminder Activity Launch  	
+    }
+    
+    private void launchJournal(){
+    	//TODO Journal Activity Launch
     }
 }
